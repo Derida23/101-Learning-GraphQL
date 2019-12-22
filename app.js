9e272ac196cd5f0fe6457fdce594da281c36b9ec
Arian Derida <arianderida@gmail.com>
2019-12-22 16:52:59 +0700

    Add : Mongoose and Configure

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const graphqlHTPP = require('express-graphql');
-const schema = require('./schema/schema')
+const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 const app = express();
+
+mongoose.connect(
+  "mongodb+srv://admin:qwerty123@niomicgraphql-b7hi3.mongodb.net/test?retryWrites=true&w=majority"
+);
+
+mongoose.connection.once('open', () => {
+    console.log('Server running with Mongoose // 4040')
+})
 
 app.use('/graphql', graphqlHTPP({
 schema,
