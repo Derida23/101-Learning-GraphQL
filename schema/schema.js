503331a9be6b0e33f574914ecf5431eca08aabdc
Arian Derida <arianderida@gmail.com>
2019-12-22 17:56:43 +0700

    Add : Insert Mutation

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,8 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 const { GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList } = graphql;
+const Jurusan = require('../models/jurusan');
+const Mahasiswa = require('../models/mahasiswa');
 
 var jurusanx = [
   {
@@ -69,7 +71,7 @@ const JurusanType = new GraphQLObjectType({
     mahasiswa: {
         type: new GraphQLList(MahasiswaType),
         resolve(parent, args){
-            return _.filter(data_mahasiswa, {jurusanid: parent.id});
+            // return _.filter(data_mahasiswa, {jurusanid: parent.id}); //mengembalikan dataDummy
         }
     }
   })
@@ -84,7 +86,7 @@ const MahasiswaType = new GraphQLObjectType({
     prodi: {
         type: JurusanType,
         resolve(parent, args){
-            return _.find(jurusanx, {id: parent.jurusanid});
+            // return _.find(jurusanx, {id: parent.jurusanid}); //mengembalikan dataDummy
         }
     }
   })
@@ -99,32 +101,69 @@ const RootQuery = new GraphQLObjectType({
       args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         //tempat mengambil data dari database
-        return _.find(jurusanx, { id: args.id }); //mengembalikan data
+        // return _.find(jurusanx, { id: args.id }); //mengembalikan dataDummy
       }
     },
     mahasiswa: {
       type: MahasiswaType,
       args: { id: { type: GraphQLID }},
       resolve(parent,args) {
-          return _.find(data_mahasiswa, { id: args.id});
+          // return _.find(data_mahasiswa, { id: args.id}); //mengembalikan dataDummy
       }
     },
     read_mahasiswa: {
       type: new GraphQLList(MahasiswaType),
       resolve(parent, args){
-        return data_mahasiswa
+        // return data_mahasiswa //mengembalikan dataDummy
       }
     },
     read_jurusan: {
       type: new GraphQLList(JurusanType),
       resolve(parent, args){
-        return jurusanx
+        // return jurusanx //mengembalikan dataDummy
       }
     }
   }
 
 });
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addJurusan: {
+      type: JurusanType,
+      args: {
+        jurusan: { type: GraphQLString },
+        kaprodi: { type: GraphQLString }
+      },
+      resolve(parent, args){
+        let jurusan = new Jurusan ({
+          jurusan: args.jurusan,
+          kaprodi: args.kaprodi
+        });
+        return jurusan.save()
+      }
+    },
+    addMahasiswa: {
+      type: MahasiswaType,
+      args: {
+        nama: { type: GraphQLString },
+        umur: { type: GraphQLInt },
+        jurusanid: { type: GraphQLID}
+      },
+      resolve(parent, args){
+        let mahasiswa = new Mahasiswa({
+          nama: args.nama,
+          umur: args.umur,
+          jurusanid: args.jurusanid
+        });
+        return mahasiswa.save()
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-    query : RootQuery
+    query : RootQuery,
+    mutation: Mutation
 });
